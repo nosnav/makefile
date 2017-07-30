7f87a7f9373fc6d3a56b21da377988e1f0252a4a
thanhduongvs <thanh.duong.vs@gmail.com>
2017-07-30 10:14:04 +0700

    them thu muc dau tien

---
+++ make01/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+	printf("Hello world. My name is Thanh\r\n");
+	printf("It is a example simple Makefile for a C program\r\n");
+	return 0;
+}
